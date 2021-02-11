eee469acfa55d5665a7da8b621e170400e5d84b2
Riley <riley@Saranis-MacBook-Pro.local>
2021-02-10 20:45:21 -0800

    added more info to header.

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-"""This is the file that Docker runs, as this project ships
+"""
+This is the file that Docker runs, as this project ships.
 
-To run a different file, change "test.py" in line 11 of the Dockerfile to
+Create trees and execute pipelines in main().
+
+An example pipeline can be found in test_tree_and_pipelines.py.
+
+To make Docker run a different file, change "test.py" in line 11 of the Dockerfile to
 the desired .py file. Make sure to import all from lib!
 """
+
+# this will import all required functions
 from lib import *
 
 
 def main():
-    return
+	# build a tree here.
 
+	print('main: Edit this file to start creating trees and executing pipelines.\n')
+
+	return
 
 if __name__ == '__main__':
     main()
